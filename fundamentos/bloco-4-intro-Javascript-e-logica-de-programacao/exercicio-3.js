fedc7f1a091f27078355f527762be959dfcfa1c0
Quezia Fonseca de Lima <qfonseca1@gmail.com>
2021-10-08 23:13:51 -0300

    exercicio 3

---
+++ fundamentos/bloco-4-intro-Javascript-e-logica-de-programacao/exercicio-3.js
@@ -0,0 +1,17 @@
+let a = 8;
+let b = 3;
+let c = 5;
+let maior;
+let resposta;
+if (a>b){
+    maior = a
+}else {
+   maior = b
+}
+if (maior>c){
+    resposta = maior
+} else{
+    resposta = c
+}
+
+console.log (resposta)
