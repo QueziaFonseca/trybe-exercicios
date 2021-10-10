871bc363a372e083303a706b04819d67ae6a6771
Quezia Fonseca de Lima <qfonseca1@gmail.com>
2021-10-10 15:47:07 -0300

    exercicio 9}

---
+++ fundamentos/bloco-4-intro-Javascript-e-logica-de-programacao/exercicio-9.js
@@ -0,0 +1,10 @@
+// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+// Bonus: use somente um if .
+let resultado = `false`;
+const a =8;
+const b =2;
+const c =20;
+ if((a%2!==0) || (b%2!==0) || (c%2!==0)){
+    resultado = `true`
+ }
+ console.log(resultado);
