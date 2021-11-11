6dfa5b56cb792e9abcf6446ddba4421fee9bb86b
quezia <qfonseca1@gmail.com>
2021-11-10 21:37:54 -0300

    substutui let por const dentro do if-else e finaliza questao 1

--- fundamentos/bloco-7-Introdução-JavaScript-ES6-e-Testes-Unitários/dia-1-JavaScript-ES6-let-const-arrow-functions-e-template-literal/exercicio-1.js
+++ fundamentos/bloco-7-Introdução-JavaScript-ES6-e-Testes-Unitários/dia-1-JavaScript-ES6-let-const-arrow-functions-e-template-literal/exercicio-1.js
@@ -10,8 +10,8 @@ const testingScope = escopo => {
       ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
       console.log(ifScope);
     } else {
-      let elseScope = `Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope)`;
+      const elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+      console.log(elseScope);
     }
     // console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
   }
