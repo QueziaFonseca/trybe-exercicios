2f9a72bf61ce55fa4678930d5fa783c84ec49d31
QueziaFonseca <qfonseca1@gmail.com>
2021-12-24 18:28:49 -0300

    renderizando lista na tela

--- Desenvolvimento-front-end/bloco_10-Introduçao-a-react/dia-1-hello-world-no-react/task-list/task-list/src/App.js
+++ Desenvolvimento-front-end/bloco_10-Introduçao-a-react/dia-1-hello-world-no-react/task-list/task-list/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 const Task = (value) => {
@@ -7,25 +7,15 @@ const Task = (value) => {
   );
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <ul> {Task('teste')}
+      </ul>
+    );
+  }
+
 }
+
 
 export default App;
