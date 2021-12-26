a176557bf590b67fac0fa006b6a548a450b9c958
QueziaFonseca <qfonseca1@gmail.com>
2021-12-25 23:47:35 -0300

    atualiza component Header.jsx

--- Desenvolvimento-front-end/bloco_10-Introduçao-a-react/dia-1-hello-world-no-react/exercicio-3_conteudo-react/conteudos-react/src/Header.jsx
+++ Desenvolvimento-front-end/bloco_10-Introduçao-a-react/dia-1-hello-world-no-react/exercicio-3_conteudo-react/conteudos-react/src/Header.jsx
@@ -3,9 +3,9 @@ import React from "react";
 class Header extends React.Component {
     render() {
         return (
-            <>
+            <header>
             <h1 className="title">Conteúdos de Front-End</h1>
-            </>
+            </header>
         )
     }
 }
