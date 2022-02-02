10a5bc55116c1f0e899aeda143aaa5e930725929
quezia-lima <qfonseca1@gmail.com>
2022-02-02 13:42:53 -0300

    adiciona arquivo App.js

--- Desenvolvimento-front-end/bloco_10-Introduçao-a-react/dia-2_componentes-react/my-pokedex/my-pokedex/src/App.js
+++ Desenvolvimento-front-end/bloco_10-Introduçao-a-react/dia-2_componentes-react/my-pokedex/my-pokedex/src/App.js
@@ -1,19 +1,18 @@
-import "./App.css";
-import React from "react";
-import pokemons from "./data";
+import React from 'react';
+import './App.css';
+import pokemons from './data';
 import Pokedex from './Pokedex';
 
 class App extends React.Component {
-  render() {
+render() {
+  return (
+    <div className="App">
+      <h1> Pokedex </h1>
+      <Pokedex pokemons={pokemons} />
+    </div>
+  );
+}
 
-
-    return (
-      <div className="App">
-        <h1> Pokedex </h1>
-      <Pokedex pokemons= {pokemons}/>
-      </div>
-    );
-  }
 }
 
 export default App;
