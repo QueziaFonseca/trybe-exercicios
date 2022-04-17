f96283535845d03da5606d10f53cf2ee2020b0d9
quezia-lima <qfonseca1@gmail.com>
2022-04-17 19:52:45 -0300

    exercicio-5: exibe a categoria do imc

---
+++ Desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio-5/imc.js
@@ -0,0 +1,15 @@
+const readline = require('readline-sync');
+
+
+function calculaImc () {
+  const peso = readline.questionFloat('Qual o seu peso? (em kg)');
+  const altura = readline.questionInt('Qual a sua altura? (em cm)');
+
+  console.log(`Peso: ${peso}, Altura: ${altura}`);
+
+  const imc = (peso / Math.pow(altura / 100, 2)).toFixed(2);
+
+  console.log(`IMC: ${imc}`);
+}
+
+calculaImc();
